7bafe5bc42fc2d290eb3f8b02b5e9800a17e8201
suryasai <suryasaigopalam@gmail.com>
2024-01-03 20:18:53 +0530

    Initial Commit

---
+++ RockPaperScissors/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RockPaperScissors
+//
+//  Created by surya sai on 03/01/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RockPaperScissors/RockPaperScissorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RockPaperScissorsApp.swift
+//  RockPaperScissors
+//
+//  Created by surya sai on 03/01/24.
+//
+
+import SwiftUI
+
+@main
+struct RockPaperScissorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
